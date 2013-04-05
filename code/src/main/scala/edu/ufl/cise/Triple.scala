b50e493574b66efc08e66d6684db63537f6b32a5
SunPHM <pengyang823@gmail.com>
2013-04-04 21:53:37 -0300

    Update Triple.scala

--- code/src/main/scala/edu/ufl/cise/Triple.scala
+++ code/src/main/scala/edu/ufl/cise/Triple.scala
@@ -2,9 +2,8 @@ package edu.ufl.cise
 
 // used to represent the entity-slot-entity triple during the sentence relation extraction
 class Triple (entity0:String, slot:String, entity1:String) {
-  override def toString():String = 
-	{
-		val s = entity0 + " - " + slot + " - " + entity1
-		return s
-	}
+	
+	override def toString():String = entity0 + " - " + slot + " - " + entity1
+	
+	def toArray(): Array[String] = Array(entity0, slot, entity1)
 }
