7b528b281ddf4bebcf6137d18cda649284b34f85
Christan Grant <christangrant@gmail.com>
2013-04-05 18:19:36 -0400

    changes so the first base line works

---
+++ code/src/main/scala/edu/ufl/cise/TRECSSF2013.scala
@@ -0,0 +1,29 @@
+package edu.ufl.cise
+
+object TRECSSF2013 extends Logging {
+
+  def main(args: Array[String]): Unit = {
+
+    Pipeline.init()
+    
+    val query = new SSFQuery("Richard Radcliffe", "topped")    
+    logInfo("SSFQuery : %s".format(query)) 
+    
+    //val z = Faucet.getStreams("2012-05-02-00", "news.f451b42043f1f387a36083ad0b089bfd.xz.gpg")
+    val z = Faucet.getStreams("2012-05-02",0,1)
+      //.take(100)
+      .map(si => new String(si.body.cleansed.array, "UTF-8").take(500))
+      //.map(_.take(964))
+      .map(Pipeline.run(_))
+      .flatMap(x => x)
+      .filter(p => 
+        p.entity0.toLowerCase.equalsIgnoreCase(query.entity.toLowerCase) //||
+        //query.entity.toLowerCase.contains(p.entity0.toLowerCase()) ||
+        //query.slotName.toLowerCase.contains(p.slot.toLowerCase()) ||
+        //p.slot.toLowerCase.equalsIgnoreCase(query.slotName.toLowerCase)
+      )
+      .foreach(t => logInfo("Answer: %s".format(t.toString)))
+ 
+  }
+
+}
--- code/src/main/scala/edu/ufl/cise/Triple.scala
+++ code/src/main/scala/edu/ufl/cise/Triple.scala
@@ -1,7 +1,7 @@
 package edu.ufl.cise
 
 // used to represent the entity-slot-entity triple during the sentence relation extraction
-class Triple (entity0:String, slot:String, entity1:String) {
+class Triple (val entity0:String, val slot:String, val entity1:String) {
 	
 	override def toString():String = entity0 + " - " + slot + " - " + entity1
 	
