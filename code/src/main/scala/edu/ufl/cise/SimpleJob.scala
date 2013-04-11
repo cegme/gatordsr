382b85f38fb09ba4161eba29610e4e372a5b608e
Christan Grant <christangrant@gmail.com>
2013-04-11 17:32:39 -0400

    small test changes

--- code/src/main/scala/edu/ufl/cise/SimpleJob.scala
+++ code/src/main/scala/edu/ufl/cise/SimpleJob.scala
@@ -5,14 +5,16 @@ import spark.SparkContext
 import spark.streaming.StreamingContext
 import SparkContext._
 
-object SimpleJob extends Application {
+object SimpleJob  {
   
+  def main(args: Array[String]) = {
   val logFile = "/var/log/syslog" // Should be some file on your system
   
-  val sc = new SparkContext("local[2]", "gatordsr", "/home/morteza/zproject/packages/spark-0.7.0/",
-    null)
+  println(">>> " + System.getenv("SPARK_HOME"))
+  val sc = new SparkContext("local[2]", "gatordsr", "/homes/cgrant/projects/spark/", Seq[String]("/home/cgrant/projects/gatordsr/code/target/scala-2.9.2/gatordsr_2.9.2-0.01.jar"))
   val logData = sc.textFile(logFile, 2).cache()
   val numAs = logData.filter(line => line.contains("a")).count()
   val numBs = logData.filter(line => line.contains("b")).count()
   println("Lines with a: %s, Lines with b: %s".format(numAs, numBs))
+  }
 }
