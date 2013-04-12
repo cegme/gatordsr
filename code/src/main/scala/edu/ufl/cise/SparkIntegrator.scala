4d8935ddaec2725d62cb92b3d0924b781985b24b
Morteza Shahriari Nia <info@MShahriariNia.com>
2013-04-12 19:18:08 -0400

    Fixed error on could not bind to port: Caused by: org.jboss.netty.channel.ChannelException: Failed to bind to: /10.136.72.0:35570

--- code/src/main/scala/edu/ufl/cise/SparkIntegrator.scala
+++ code/src/main/scala/edu/ufl/cise/SparkIntegrator.scala
@@ -1,7 +1,17 @@
 package edu.ufl.cise
+
+import spark.streaming.StreamingContext
+import spark.SparkContext
+import spark.streaming.Seconds
 /**
  * Here we will add the functionality to utilize spark features.
  */
 object SparkIntegrator {
 
+  val sc = new SparkContext("local[2]", "gatordsr", "$YOUR_SPARK_HOME",
+    List("target/scala-2.9.2/gatordsr_2.9.2-0.01.jar"))
+//  val ssc = new StreamingContext("local[2]", "gatordsrStreaming", Seconds(2),
+//    "$YOUR_SPARK_HOME", List("target/scala-2.9.2/gatordsr_2.9.2-0.01.jar"))
+  val NUM_SLICES = 2
+  
 }
