1af0d4c7b8070c4b5f1ab7aed94beb558bc238eb
Morteza Shahriari Nia <info@MShahriariNia.com>
2013-04-12 18:46:24 -0400

    Collect SparkContext and its configs at SparkIntegrator.

--- code/src/main/scala/edu/ufl/cise/TRECSSF2013.scala
+++ code/src/main/scala/edu/ufl/cise/TRECSSF2013.scala
@@ -10,10 +10,8 @@ object TRECSSF2013 extends Logging {
 
     val text = "Abraham Lincoln was the 16th President of the United States, serving from March 1861 until his assassination in April 1865."
     val query = new SSFQuery("Abraham Lincoln", "president of")
-    val pipeline = Pipeline.getPipeline(text, query)
-    pipeline.run(text)
     val pipeline = Pipeline.getPipeline(query)
-    pipeline.run(text, sc)
+    pipeline.run(text, SparkIntegrator.sc)
 
 //    new SSFQuery("Richard Radcliffe", "topped")
     //    logInfo("SSFQuery : %s".format(query))
@@ -22,7 +20,7 @@ object TRECSSF2013 extends Logging {
       //.take(100)
       .map(si => new String(si.body.cleansed.array, "UTF-8"))
       //.map(_.take(964))
-      .map(pipeline.run(_).toArray())
+      .map(pipeline.run(_, SparkIntegrator.sc).toArray())
       .flatMap(x => x)
       .filter(p =>
         p.asInstanceOf[Triple].entity0.toLowerCase.equalsIgnoreCase(query.entity.toLowerCase) //||
