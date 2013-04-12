55a773a9c34f709810339e2960dbf47c798aebc0
Morteza Shahriari Nia <info@MShahriariNia.com>
2013-04-12 18:46:24 -0400

    Collect SparkContext and its configs at SparkIntegrator.

--- code/src/main/scala/edu/ufl/cise/SparkIntegrator.scala
+++ code/src/main/scala/edu/ufl/cise/SparkIntegrator.scala
@@ -10,8 +10,8 @@ object SparkIntegrator {
 
   val sc = new SparkContext("local[2]", "gatordsr", "$YOUR_SPARK_HOME",
     List("target/scala-2.9.2/gatordsr_2.9.2-0.01.jar"))
-//  val ssc = new StreamingContext("local[2]", "gatordsrStreaming", Seconds(2),
-//    "$YOUR_SPARK_HOME", List("target/scala-2.9.2/gatordsr_2.9.2-0.01.jar"))
+  val ssc = new StreamingContext("local[2]", "gatordsrStreaming", Seconds(2),
+    "$YOUR_SPARK_HOME", List("target/scala-2.9.2/gatordsr_2.9.2-0.01.jar"))
   val NUM_SLICES = 2
   
 }
