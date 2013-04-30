d5198a44b58ae2fe3346d64c225658926dc753a2
Morteza Shahriari Nia <info@MShahriariNia.com>
2013-04-29 21:38:01 -0400

    S3 Downloader.

--- code/src/main/java/fileProcessor/FileProcessor.java
+++ code/src/main/java/fileProcessor/FileProcessor.java
@@ -4,47 +4,139 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
 import java.net.URL;
-
-import org.apache.commons.io.IOUtils;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class FileProcessor {
 
-	public static void downloadFile(URL from, File to, boolean overwrite) throws Exception {
-	    if (to.exists()) {
-	        if (!overwrite)
-	            throw new Exception("File " + to.getAbsolutePath() + " exists already.");
-	        if (!to.delete())
-	            throw new Exception("Cannot delete the file " + to.getAbsolutePath() + ".");
-	    }
+	public static void main(String[] args) {
+		URL url;
+		InputStream is = null;
+		BufferedReader br;
+		BufferedReader br2;
 
-	    int lengthTotal = 0;
-	    try {
-	        HttpURLConnection content = (HttpURLConnection) from.openConnection();
-	        lengthTotal = content.getContentLength();
-	    } catch (Exception e) {
-	        lengthTotal = -1;
-	    }
+		try {
+			url = new URL(
+					"http://s3.amazonaws.com/aws-publicdatasets/trec/kba/kba-streamcorpus-2013-v0_2_0-english-and-unknown-language/index.html");
+			is = url.openStream(); // throws an IOException
+			br = new BufferedReader(new InputStreamReader(is));
 
-	    int lengthSoFar = 0;
-	    InputStream is = from.openStream();
-	    
-	    String[] gpgCommands = new String[] {
-	            "gpg",
-	            "--passphrase",
-	            "password",
-	            "--decrypt",
-	            "test-files/accounts.txt.gpg"
-	    };
+			String s = "";
+			int threadCount = 32;
+			ExecutorService executor = Executors
+					.newFixedThreadPool(threadCount);
 
-	    Process gpgProcess = Runtime.getRuntime().exec(gpgCommands);
-	    BufferedReader gpgOutput = new BufferedReader(new InputStreamReader(gpgProcess.getInputStream()));
-	    BufferedReader gpgError = new BufferedReader(new InputStreamReader(gpgProcess.getErrorStream()));
-	    byte[] bytes = IOUtils.toByteArray(is);
-	    
-	    
-	    is.close();
-	    
+			while (true) {
+
+				// Thread t1 = myThread(br.readLine());
+
+				Thread t = null;
+				Pattern p = Pattern.compile("a href=\"([^\"]+)\"");
+				final Matcher m1 = p.matcher(br.readLine());
+
+				if (m1.find()) {
+					String linkStr = m1.group(1);
+					System.out.println(linkStr);
+					final String dir = linkStr.substring(0,
+							linkStr.indexOf('/'));
+					System.out.println(dir);
+
+					Runnable worker = new Thread("" + linkStr) {
+						public void run() {
+							downloadDir(dir);
+						};
+					};
+					executor.execute(worker);
+				}
+
+				executor.shutdown();
+				while (!executor.isTerminated()) {
+				}
+				System.out.println("Finished all threads");
+			}
+
+		} catch (Exception ioe) {
+			ioe.printStackTrace();
+		}
+	}
+
+	// static Thread myThread(String line) {
+	// Thread t = null;
+	// Pattern p = Pattern.compile("a href=\"([^\"]+)\"");
+	// final Matcher m1 = p.matcher(line);
+	// if (m1.find()) {
+	// t = new Thread() {
+	// public void run() {
+	// String linkStr = m1.group(1);
+	// System.out.println(linkStr);
+	// String dir = linkStr.substring(0, linkStr.indexOf('/'));
+	// System.out.println(dir);
+	// downloadDir(dir);
+	// };
+	// };
+	// t.start();
+	// }
+	// return t;
+	// }
+
+	static void downloadDir(String dir) {
+		try {
+			String localDir = "/media/sdd/corpus/";
+
+			(new File(localDir + dir)).mkdirs();
+
+			String line;
+
+			URL url = new URL(
+					"http://s3.amazonaws.com/aws-publicdatasets/trec/kba/kba-streamcorpus-2013-v0_2_0-english-and-unknown-language/"
+							+ dir + "/index.html");
+			InputStream is = url.openStream(); // throws an IOException
+			BufferedReader br2 = new BufferedReader(new InputStreamReader(is));
+
+			Pattern p = Pattern.compile("a href=\"([^\"]+)\"");
+			while ((line = br2.readLine()) != null) {
+				Matcher m = p.matcher(line);
+				if (m.find()) {
+					String linkStr = m.group(1);
+					if (linkStr.contains("gpg")) {
+						System.out.println(linkStr);
+						String gpgStr = "http://s3.amazonaws.com/aws-publicdatasets/trec/kba/kba-streamcorpus-2013-v0_2_0-english-and-unknown-language/"
+								+ dir + "/" + linkStr;
+
+						String command = "wget -O - "
+								+ gpgStr
+								+ " |   gpg --no-permission-warning --trust-model always --output "
+								+ localDir + dir + "/"
+								+ linkStr.substring(0, linkStr.length() - 4);
+
+						String[] cmd = { "/bin/sh", "-c", command };
+
+						System.out.println(command);
+						Process process = Runtime.getRuntime().exec(cmd);
+						// System.out.println(process.exitValue());
+						System.out.println(process.toString());
+						BufferedReader stdOut = new BufferedReader(
+								new InputStreamReader(process.getInputStream()));
+						BufferedReader stdErr = new BufferedReader(
+								new InputStreamReader(process.getErrorStream()));
+						while ((line = stdOut.readLine()) != null) {
+							System.out.println(line);
+						}
+
+						System.out.println("///////////////////////////////");
+						while ((line = stdErr.readLine()) != null) {
+							System.out.println(line);
+						}
+						process.destroy();
+					}
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
 	}
 }
