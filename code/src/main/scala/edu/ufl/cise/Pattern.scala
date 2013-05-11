7eb23df038e249e45877f3624f7704855dfbba9c
SunPHM <pengyang823@gmail.com>
2013-05-10 20:35:55 -0400

    proto-pattern

---
+++ code/src/main/scala/edu/ufl/cise/Pattern.scala
@@ -0,0 +1,32 @@
+package edu.ufl.cise
+
+import java.util.ArrayList
+
+object Pattern {
+  
+  val entity_list = new ArrayList[Entity]
+  val slot_list = new ArrayList[Slot]
+  val pattern_list = new ArrayList[Pattern]
+  
+  // initialize entity_list, slot_list, pattern_list
+  def init(){
+    
+  }
+  
+}
+
+class Pattern(regex:String){
+  
+  
+}
+
+class Entity(addr:String, names:Array[String]){
+  
+  
+  
+}
+
+class Slot{
+  
+  
+}
