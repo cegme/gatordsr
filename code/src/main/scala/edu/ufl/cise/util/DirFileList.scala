2d72d630bb25f7b6557d98c3f54d54e36d198a73
Morteza Shahriari Nia <info@MShahriariNia.com>
2013-05-14 19:47:13 -0400

    Local file explorer.

---
+++ code/src/main/scala/edu/ufl/cise/util/DirFileList.scala
@@ -0,0 +1,39 @@
+package edu.ufl.cise.util
+
+import java.io.IOException
+import java.nio.file.Files
+import java.nio.file.Path
+import java.nio.file.Paths
+import java.nio.file.SimpleFileVisitor
+
+class DirFileList {
+
+  def main(args: Array[String]): Unit = {
+    
+//    try {
+//    val startPath = Paths.get("/home/morteza/Desktop");
+//    Files.walkFileTree(startPath, new SimpleFileVisitor<Path>() {
+//        @Override
+//        public FileVisitResult preVisitDirectory(Path dir,
+//                BasicFileAttributes attrs) {
+//            System.out.println("Dir: " + dir.toString());
+//            return FileVisitResult.CONTINUE;
+//        }
+//
+//        @Override
+//        public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) {
+//            System.out.println("File: " + file.toString());    
+//            return FileVisitResult.CONTINUE;
+//        }
+//
+//        @Override
+//        public FileVisitResult visitFileFailed(Path file, IOException e) {
+//            return FileVisitResult.CONTINUE;
+//        }
+//    });
+//} catch (IOException e) {
+//    e.printStackTrace();
+//}
+  }
+
+}
