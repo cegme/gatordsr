b61d4f68e99bc24cf6a224b83c29cd2760b9750d
Morteza Shahriari Nia <info@MShahriariNia.com>
2013-05-16 12:45:38 -0400

    Fixing EmbededFaucet Report.

--- code/src/main/scala/edu/ufl/cise/EmbededFaucet.scala
+++ code/src/main/scala/edu/ufl/cise/EmbededFaucet.scala
@@ -5,23 +5,22 @@ import java.io.ByteArrayOutputStream
 import java.text.DecimalFormat
 import java.text.SimpleDateFormat
 import java.util.concurrent.atomic.AtomicInteger
-
 import scala.collection.JavaConversions.asScalaBuffer
 import scala.sys.process.stringToProcess
-
 import org.apache.thrift.protocol.TBinaryProtocol
 import org.apache.thrift.transport.TIOStreamTransport
 import org.apache.thrift.transport.TTransportException
-
 import edu.ufl.cise.util.StreamItemWrapper
 import streamcorpus.StreamItem
 import fileproc.DirList
+import scala.collection.parallel.immutable.ParSeq
 
 object EmbededFaucet extends Logging {
 
   //val DIRECTORY = "/home/morteza/2013Corpus/s3.amazonaws.com/aws-publicdatasets/trec/kba/kba-streamcorpus-2013-v0_2_0-english-and-unknown-language/"
   val DIRECTORY = "/media/sdd/s3.amazonaws.com/aws-publicdatasets/trec/kba/kba-streamcorpus-2013-v0_2_0-english-and-unknown-language/"
-    val FILTER = "2012-01-0"
+  val FILTER = "2012-01-0"
+    val query = "president"
 
   val numberFormatter = new DecimalFormat("00")
 
@@ -44,17 +43,17 @@ object EmbededFaucet extends Logging {
     logInfo(fileName)
     val baos = new java.io.ByteArrayOutputStream
     //println(("sshpass -p 'trecGuest' ssh trecGuest@sm321-01.cise.ufl.edu 'cat " + fileName + "'") #| (" gpg -q --no-verbose --no-permission-warning --trust-model always --output - --decrypt " + fileName) #| "xz --decompress" )
-    ("sshpass -p 'trecGuest' ssh trecGuest@sm321-01.cise.ufl.edu 'cat /media/sde/s3.amazonaws.com/aws-publicdatasets/trec/kba/kba-streamcorpus-2013-v0_2_0-english-and-unknown-language/2012-11-03-05/WEBLOG-89-15957f5baef21e2cda6dca887b96e23e-e3bb3adf7504546644d4bc2d62108064.sc.xz.gpg' ") #| (" gpg -q --no-verbose --no-permission-warning --trust-model always --output - --decrypt " + fileName) #| " xz --decompress" #>  baos !;
+    ("sshpass -p 'trecGuest' ssh trecGuest@sm321-01.cise.ufl.edu 'cat /media/sde/s3.amazonaws.com/aws-publicdatasets/trec/kba/kba-streamcorpus-2013-v0_2_0-english-and-unknown-language/2012-11-03-05/WEBLOG-89-15957f5baef21e2cda6dca887b96e23e-e3bb3adf7504546644d4bc2d62108064.sc.xz.gpg' ") #| (" gpg -q --no-verbose --no-permission-warning --trust-model always --output - --decrypt " + fileName) #| " xz --decompress" #> baos !;
     baos
   }
 
-  def getStreams(date: String, hour: Int, fileName: String, data: ByteArrayOutputStream): List[StreamItemWrapper] = {
+  def getStreams(date: String, hour: Int, fileName: String, data: ByteArrayOutputStream): ParSeq[StreamItemWrapper] = {
     val bais = new ByteArrayInputStream(data.toByteArray())
     val transport = new TIOStreamTransport(bais)
     transport.open()
     val protocol = new TBinaryProtocol(transport)
 
-    var list = List[StreamItemWrapper]()
+    var list = List[StreamItemWrapper]().par
 
     var index: Int = 0
     var exception = false
@@ -71,7 +70,7 @@ object EmbededFaucet extends Logging {
           logDebug("Error in mkStreamItem") //TODO stop before exception
           exception = true
       }
-      list = new StreamItemWrapper(date, hour, fileName, index, si) :: list
+      list = list :+ new StreamItemWrapper(date, hour, fileName, index, si)
       index = index + 1
     }
     transport.close()
@@ -83,8 +82,9 @@ object EmbededFaucet extends Logging {
     println("total file count on disk sdd is: " + fileList.size)
 
     val siCount = new AtomicInteger(0)
+    val siFilteredCount = new AtomicInteger(0)
 
-    val tempFilter = fileList.map(p => {
+    val tempFilter = fileList.foreach(p => {
       val temp = p.asInstanceOf[String]
       val pattern = """.*language/([^/]+)-(.+)/(.+)""".r
 
@@ -92,34 +92,36 @@ object EmbededFaucet extends Logging {
       val day = dayHourFileList.apply(0).group(1)
       val hour = new Integer(dayHourFileList.apply(0).group(2))
       val fileName = dayHourFileList.apply(0).group(3)
-        val data = grabGPGLocal(day, hour, temp)
-     // val data = grabGPGSSH("/media/sde/s3.amazonaws.com/aws-publicdatasets/trec/kba/kba-streamcorpus-2013-v0_2_0-english-and-unknown-language/2012-11-03-05/WEBLOG-89-15957f5baef21e2cda6dca887b96e23e-e3bb3adf7504546644d4bc2d62108064.sc.xz.gpg")
+      val data = grabGPGLocal(day, hour, temp)
+      // val data = grabGPGSSH("/media/sde/s3.amazonaws.com/aws-publicdatasets/trec/kba/kba-streamcorpus-2013-v0_2_0-english-and-unknown-language/2012-11-03-05/WEBLOG-89-15957f5baef21e2cda6dca887b96e23e-e3bb3adf7504546644d4bc2d62108064.sc.xz.gpg")
       val tempArr = data.toByteArray()
       val sis = getStreams(day, hour, fileName, data)
       siCount.addAndGet(sis.size)
-      sis
-    }).flatMap(x => x).filter(p => {
-      var res = false
 
-      if (p.streamItem.body != null) {
-        val document = p.streamItem.body.getClean_visible()
-        if (document != null) {
-          val strEnglish = document.toLowerCase().replaceAll("[^A-Za-z0-9\\p{Punct}]", " ").replaceAll("\\s+", " ")
-            .replaceAll("(\r\n)+", "\r\n").replaceAll("(\n)+", "\n").replaceAll("(\r)+", "\r").toLowerCase()
-          res = strEnglish.contains("oosevelt")
-        } else
-          res = false
-      }
-      //      if(res == false)
-      //        println(p.streamItem)
-      res
+      sis.foreach(p => {
+        var res = false
+
+        if (p.streamItem.body != null) {
+          val document = p.streamItem.body.getClean_visible()
+          if (document != null) {
+            val strEnglish = document.toLowerCase().replaceAll("[^A-Za-z0-9\\p{Punct}]", " ").replaceAll("\\s+", " ")
+              .replaceAll("(\r\n)+", "\r\n").replaceAll("(\n)+", "\n").replaceAll("(\r)+", "\r").toLowerCase()
+            res = strEnglish.contains("oosevelt")
+          } else
+            res = false
+        }
+        if (res == true){
+          println(p)
+          siFilteredCount.incrementAndGet()
+        }
+      })
     })
 
     println("total file count on disk" + DIRECTORY + " before filter is: " + siCount.get())
-    println("total file count on disk " + DIRECTORY + "after filter is: " + tempFilter.size)
-    tempFilter.foreach(p => {
-      logInfo(p.toString())
-    })
+     println("total file count on disk " + DIRECTORY + "after filter is: " + siFilteredCount.get())
+    //    tempFilter.foreach(p => {
+    //      logInfo(p.toString())
+    //    })
   }
 
 }
--- code/src/main/scala/edu/ufl/cise/util/StreamItemWrapper.scala
+++ code/src/main/scala/edu/ufl/cise/util/StreamItemWrapper.scala
@@ -5,17 +5,31 @@ import edu.ufl.cise.EmbededFaucet
 
 class StreamItemWrapper(val day: String, val hour: Int, val fileName: String, val index: Int, val streamItem: StreamItem) extends Serializable {
 
-   def getDirectoryName(day: String, hour: Int): String = {
+  def getDirectoryName(day: String, hour: Int): String = {
     val hourStr = EmbededFaucet.numberFormatter.format(hour)
     "%s-%s".format(day, hourStr)
   }
-  
+
   override def toString = {
-    val str = getDirectoryName(day, hour) + "/" + fileName + "/" + index 
-//    if (streamItem.getBody != null && streamItem.getBody.getClean_visible() != null)
-//      str + ":" + streamItem.getBody.getClean_visible.substring(0, 30)
-//    else
-      str
+    var str = getDirectoryName(day, hour) + "/" + fileName + "[" + index + "]"
+    //    if (streamItem.getBody != null && streamItem.getBody.getClean_visible() != null)
+    //      str + ":" + streamItem.getBody.getClean_visible.substring(0, 30)
+    //    else
+
+    val doc = streamItem.getBody.getClean_visible()
+    var i = -1
+    var initIndex = 0
+    var endIndex = doc.length()
+    while ((i = doc.indexOf(EmbededFaucet.query, i + 1)) != -1) {
+      initIndex = i - 30
+      if (initIndex < 0)
+        initIndex = 0
+      endIndex = i + EmbededFaucet.query.length
+      if (endIndex > doc.length())
+        endIndex = doc.length() - 1
+      str = str + doc.substring(initIndex, endIndex)
+    }
+    str
   }
 }
 
