d7d5e71bd1202bf85e9603f1094e793cfd306b02
SunPHM <pengyang823@gmail.com>
2013-05-25 22:50:59 -0400

    some cleanups

--- code/src/main/scala/edu/ufl/cise/pipeline/Pattern.scala
+++ code/src/main/scala/edu/ufl/cise/pipeline/Pattern.scala
@@ -1,9 +1,26 @@
 package edu.ufl.cise.pipeline
 
 import edu.ufl.cise.Triple
+import java.lang._
+import java.util.ArrayList
 
-class Pattern(entity : String, id : Int, slot : String, sid:Int, pattern : String, dir : java.lang.Integer){
+class Pattern(eid : Int, sid : Int, pattern : String, dir : Integer){
+  var entity : String = null
+  var slot : String = null
+}
+
+class Entity(entity_type:String, group:String, topic_id:String){
+
+  val names = new ArrayList[String] // the list of alias names for Entity
   
+  def add(name:String) = names.add(name) // add one more alias name for the entity
+  
+}
 
-     
+class Slot(entity_type : String, slot:String){
+
+  val names = new ArrayList[String] // the list of alias names for Slot, extracted from the WordNet
+  
+  def add(name:String) = names.add(name) // add one more alias name for the slot
+  
 }
