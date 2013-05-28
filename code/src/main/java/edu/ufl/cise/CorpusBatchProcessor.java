ceddf6277b75c8fffb4f4a5abbc6dff15740565a
Morteza Shahriari Nia <info@MShahriariNia.com>
2013-05-27 23:18:49 -0400

    Multi-threading.

--- code/src/main/java/edu/ufl/cise/CorpusBatchProcessor.java
+++ code/src/main/java/edu/ufl/cise/CorpusBatchProcessor.java
@@ -9,11 +9,13 @@ import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.Iterator;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.atomic.AtomicLong;
 import java.util.regex.Pattern;
 
@@ -70,6 +72,10 @@ public class CorpusBatchProcessor {
 																												Pipeline.queries(), Pipeline.dirs());
 	final Pattern										pattern						= Pattern.compile(query);
 
+	DecimalFormat										numberFormatter		= new DecimalFormat("00");
+
+	List<Entity>										listEntity;
+
 	/**
 	 * gets the index of thus process and total # of processes that this process
 	 * is a member of to avoid duplicate process of corpus files.
@@ -85,6 +91,14 @@ public class CorpusBatchProcessor {
 		this.totalNumProcesses = -1;
 	}
 
+	/**
+	 * Grab content of a local GPG file.
+	 * 
+	 * @param date
+	 * @param fileName
+	 * @param fileStr
+	 * @return
+	 */
 	private static InputStream grabGPGLocal(String date, String fileName, String fileStr) {
 		// System.out.println(date + "/" + fileName);
 		String command = "gpg -q --no-verbose --no-permission-warning --trust-model always --output - --decrypt "
@@ -93,6 +107,15 @@ public class CorpusBatchProcessor {
 		return FileProcessor.runBinaryShellCommand(command);
 	}
 
+	/**
+	 * Get Streams of a specific file name in a day-hour directory.
+	 * 
+	 * @param day
+	 * @param hour
+	 * @param fileName
+	 * @param is
+	 * @throws Exception
+	 */
 	private void getStreams(String day, int hour, String fileName, InputStream is) throws Exception {
 		XZCompressorInputStream bais = new XZCompressorInputStream(is);
 		TIOStreamTransport transport = new TIOStreamTransport(bais);
@@ -106,7 +129,7 @@ public class CorpusBatchProcessor {
 				StreamItem si = new StreamItem();
 				if (protocol.getTransport().isOpen())
 					si.read(protocol);
-				 siCount.incrementAndGet();
+				siCount.incrementAndGet();
 				// si.getBody().unsetRaw();
 
 				// processTokens(si);
@@ -127,6 +150,11 @@ public class CorpusBatchProcessor {
 		transport.close();
 	}
 
+	/**
+	 * Get the appropirate cause of exception string for TTransportException
+	 * 
+	 * @param e
+	 */
 	private static void tTransportExceptionPrintString(TTransportException e) {
 		switch (e.getType()) {
 		case TTransportException.ALREADY_OPEN:
@@ -147,15 +175,26 @@ public class CorpusBatchProcessor {
 		}
 	}
 
-	DecimalFormat	numberFormatter	= new DecimalFormat("00");
-
-	List<Entity>	listEntity;
-
+	/**
+	 * Process StreamItemWrapper by going through tokens and concatenating tehm to
+	 * make sure we can handle multi word entities.
+	 * 
+	 * @param siw
+	 */
 	private void process(SIWrapper siw) {
 		boolean printedFileName = false;
 		if (siw.getStreamItem().getBody() != null) {
 
 			List<Sentence> listSentence = siw.getStreamItem().getBody().getSentences().get("lingpipe");
+
+			// /TODO a map from entoty to boolean true false takes less memory than
+			// initiing all sentences.
+			List<String> listStr = new LinkedList<String>();
+			for (Sentence sentence : listSentence) {
+				String sentenceStr = pipe.transform(sentence.tokens.toArray(new Token[sentence.tokens
+						.size()]));
+				listStr.add(sentenceStr.toLowerCase());
+			}
 
 			// match all entities
 			for (Entity entity : listEntity) {
@@ -163,33 +202,26 @@ public class CorpusBatchProcessor {
 				// match all aliases of entity
 				for (int ientity = 0; ientity < entity.names().size(); ientity++) {
 					// for all sentences
-					for (int isentence = 0; isentence < listSentence.size() && !matchedEntity; isentence++) {
-						Sentence s = listSentence.get(isentence);
-						// pipe.transform(s.tokens.toArray(Token.class));
-						// for all tokens
-						for (int itokens = 0; itokens < s.tokens.size() && !matchedEntity; itokens++) {
-							String token = s.tokens.get(itokens).getToken();
-							if (token != null) {
-								token = token.toLowerCase();
+					for (int isentence = 0; isentence < listStr.size() && !matchedEntity; isentence++) {
+						String s = listStr.get(isentence);
 
-								String alias = entity.names().get(ientity);
-								if (token.equals(alias)) {
-									if (!printedFileName) {
-										System.out.print(">" + siw.day + " | " + siw.fileName + " | " + siw.getIndex()
-												+ " | " + siw.getStreamItem().getDoc_id() + " | ");
-										printedFileName = true;
-										matchedEntity = true;
-										
-									}
-
-									System.out.print(entity.topic_id() + ", ");
-									siFilteredCount.incrementAndGet();
-								}
+						String alias = entity.names().get(ientity);
+						if (s.contains(alias)) {
+							if (!printedFileName) {
+								System.out.print(">" + siw.day + " | " + siw.fileName + " | " + siw.getIndex()
+										+ " | " + siw.getStreamItem().getDoc_id() + " | ");
+								printedFileName = true;
 							}
+							matchedEntity = true;
+
+							System.out.print(entity.topic_id() + ", ");
+							siFilteredCount.incrementAndGet();
 						}
 					}
 				}
 			}
+			// }
+			// }
 			if (printedFileName)
 				System.out.println(); // final new line
 		}
@@ -200,6 +232,11 @@ public class CorpusBatchProcessor {
 		// .replaceAll("(\r)+", "\r").toLowerCase();
 	}
 
+	/**
+	 * Process StreamItem and go through tokens.
+	 * 
+	 * @param si
+	 */
 	private void process(StreamItem si) {
 		Map<String, List<Sentence>> sentencesMap = si.getBody().getSentences();
 		Set<String> sentenceSetKeys = sentencesMap.keySet();
@@ -227,6 +264,7 @@ public class CorpusBatchProcessor {
 	}
 
 	/**
+	 * Process using java ExecutorService
 	 * 
 	 * @throws ParseException
 	 */
@@ -315,6 +353,99 @@ public class CorpusBatchProcessor {
 		report(logTimeFormat, "Finished all threads");
 	}
 
+	/**
+	 * Process in multithreaded fashion.
+	 * 
+	 * @throws ParseException
+	 */
+	private void processMultiThreads() throws ParseException {
+		System.out.println("processMultiThreads");
+		final int threadCount = 31;
+
+		final Calendar cStart = Calendar.getInstance();
+		final Calendar cEnd = Calendar.getInstance();
+
+		File f = new File(CORPUS_DIR_LOCAL);
+		boolean localRun = f.exists();
+		final String DIRECTORY = (localRun) ? CORPUS_DIR_LOCAL : CORPUS_DIR_SERVER;
+		if (localRun) {
+			System.out.println("Local run.");
+			// c.setTime(format.parse("2011-10-05-00"));
+			cStart.setTime(format.parse("2011-10-07-13"));
+			cEnd.setTime(format.parse("2011-10-07-14"));
+		} else {
+			System.out.println("Server run.");
+			// c.setTime(format.parse("2011-10-05-00"));
+			cStart.setTime(format.parse("2012-08-18-01"));
+			cEnd.setTime(format.parse("2012-08-18-01"));
+		}
+
+		final AtomicInteger finishedThreadTracker = new AtomicInteger(0);
+		for (int i = 0; i < threadCount; i++) {
+			final int threadIndex = i;
+			final Calendar cTemp = Calendar.getInstance();
+			cTemp.setTime(cStart.getTime());
+			cTemp.add(Calendar.HOUR, threadIndex);
+
+			Thread worker = new Thread() {
+				public void run() {
+
+					// System.out.println(threadIndex + ": " +
+					// format.format(cTemp.getTime()) + " "
+					// + format.format(cEnd.getTime()));
+					while (!(cTemp.getTime().compareTo(cEnd.getTime()) > 0)) {
+						try {
+							final String date = format.format(cTemp.getTime());
+
+							final List<String> fileList = DirList.getFileList(DIRECTORY + date, FILTER);
+							for (final String fileStr : fileList) {
+								final int hour = cTemp.get(Calendar.HOUR_OF_DAY);
+								final String fileName = fileStr.substring(fileStr.lastIndexOf('/') + 1);
+								try {
+									InputStream is = grabGPGLocal(date, fileName, fileStr);
+									getStreams(date, hour, fileName, is);
+									is.close();
+
+									fileCount.incrementAndGet();
+									long size = FileProcessor.getLocalFileSize(fileStr);
+									processedSize.addAndGet(size);
+									report(logTimeFormat, "Thread(" + threadIndex + ")" + date + "/" + fileName);
+								} catch (Exception e) {
+									e.printStackTrace();
+								}
+							}
+						} catch (Exception e1) {
+							e1.printStackTrace();
+						}
+						cTemp.add(Calendar.HOUR, threadCount);
+						// System.out.println(threadIndex + ": " +
+						// format.format(cTemp.getTime()) + " "
+						// + format.format(cEnd.getTime()));
+					}
+					finishedThreadTracker.incrementAndGet();
+				}
+			};
+			worker.start();
+		}
+		while (finishedThreadTracker.get() < threadCount) {
+			try {
+				Thread.sleep(500);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+
+		report(logTimeFormat, "Finished all threads");
+	}
+
+	/**
+	 * Generate a timely statistics of the # of fiels, total file size processed
+	 * so far. # of StreamItems, # of Stream Items that contained an entity,
+	 * current file name etc.
+	 * 
+	 * @param df
+	 * @param message
+	 */
 	private void report(DateFormat df, String message) {
 		System.out.println(df.format(new Date()) + " Total " + fileCount + " Files "
 				+ FileProcessor.fileSizeToStr(processedSize.get(), "MB") + " SIs: " + siCount.get()
@@ -331,7 +462,8 @@ public class CorpusBatchProcessor {
 		if (args.length == 0) {
 			CorpusBatchProcessor cps = new CorpusBatchProcessor();
 			cps.listEntity = Preprocessor.entity_list();
-			cps.process();
+			// cps.process();
+			cps.processMultiThreads();
 		} else if (args.length == 2) {
 			CorpusBatchProcessor cps = new CorpusBatchProcessor(Integer.parseInt(args[0]),
 					Integer.parseInt(args[0]));
