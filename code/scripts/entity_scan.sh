cba1eefed3939d1178c473af38c723ba5984dae5
Christan Grant <christangrant@gmail.com>
2013-05-30 17:57:44 -0400

    made the entity match only earch for 1 item, not count. It continues even if blank stream item. Added a file_size_check.py to measure the size of a file given its name

--- code/scripts/entity_scan.sh
+++ code/scripts/entity_scan.sh
@@ -1,16 +1,13 @@
-
-#ls -lrt -d -1 $PWD/*.gpg | head
-#ls -lU -d -1 /media/sd{d,e}/s3.amazonaws.com/aws-publicdatasets/trec/kba/kba-streamcorpus-2013-v0_2_0-english-and-unknown-language/????-??-??-??/*.gpg | head
-#for gpg_file in /media/sd{d,e}/s3.amazonaws.com/aws-publicdatasets/trec/kba/kba-streamcorpus-2013-v0_2_0-english-and-unknown-language/????-??-??-??/*.gpg do
-#  echo ls
-#done
+# Run this from inside of the gatordsr/code/scripts/ folder
 
 # Export file so the cpp file can read the libthrift-0.9.0.so
 export LD_LIBRARY_PATH=/usr/local/lib
 
 # Run it!
-time find /media/sd{d,e}/s3.amazonaws.com/ -name '*.gpg' | head -n1000\
-  | parallel -j 128 --eta "gpg --quiet --no-permission-warning --trust-model always --decrypt {} | xz --decompress --stdout | ../cpp/main '{}'"
-  #| parallel --eta "xz --decompress < gpg --no-permission-warning --trust-model always --decrypt {} | ../cpp/main '{}'"
+#time find /media/sd{d,e}/s3.amazonaws.com/ -name '*.gpg'\
+#time ls /media/sdd/s3.amazonaws.com/aws-publicdatasets/trec/kba/kba-streamcorpus-2013-v0_2_0-english-and-unknown-language/2012-08-18-01/*.gpg\
+#time ls /media/sdd/s3.amazonaws.com/aws-publicdatasets/trec/kba/kba-streamcorpus-2013-v0_2_0-english-and-unknown-language/2012-08-18-??/*.gpg\
+time find /media/sd{d,e}/s3.amazonaws.com/ -name '*.gpg'\
+  | parallel -u -j+0 --progress "gpg --quiet --no-permission-warning --trust-model always --decrypt {} | xz --decompress --stdout | ../cpp/main '{}'"
 
 
