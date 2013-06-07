ab8bb98b941fde0a082e4376c8c9946ba80cf888
mshahriarinia <info@mshahriarinia.com>
2013-06-07 12:35:25 -0400

    Total Entity run.

---
+++ code/scriptfull.sh
@@ -0,0 +1,12 @@
+sbt 'run-main edu.ufl.cise.pipeline.Pipeline /tmp/mytempfull/x01 resources/test/kbaoutput/fx01' > /tmp/mytempfull/fx01.out 2>&1 &
+sbt 'run-main edu.ufl.cise.pipeline.Pipeline /tmp/mytempfull/x02 resources/test/kbaoutput/fx02' > /tmp/mytempfull/fx02.out 2>&1 &
+sbt 'run-main edu.ufl.cise.pipeline.Pipeline /tmp/mytempfull/x03 resources/test/kbaoutput/fx03' > /tmp/mytempfull/fx03.out 2>&1 &
+sbt 'run-main edu.ufl.cise.pipeline.Pipeline /tmp/mytempfull/x04 resources/test/kbaoutput/fx04' > /tmp/mytempfull/fx04.out 2>&1 &
+sbt 'run-main edu.ufl.cise.pipeline.Pipeline /tmp/mytempfull/x05 resources/test/kbaoutput/fx05' > /tmp/mytempfull/fx05.out 2>&1 &
+sbt 'run-main edu.ufl.cise.pipeline.Pipeline /tmp/mytempfull/x06 resources/test/kbaoutput/fx06' > /tmp/mytempfull/fx06.out 2>&1 &
+sbt 'run-main edu.ufl.cise.pipeline.Pipeline /tmp/mytempfull/x07 resources/test/kbaoutput/fx07' > /tmp/mytempfull/fx07.out 2>&1 &
+sbt 'run-main edu.ufl.cise.pipeline.Pipeline /tmp/mytempfull/x08 resources/test/kbaoutput/fx08' > /tmp/mytempfull/fx08.out 2>&1 &
+sbt 'run-main edu.ufl.cise.pipeline.Pipeline /tmp/mytempfull/x09 resources/test/kbaoutput/fx09' > /tmp/mytempfull/fx09.out 2>&1 &
+sbt 'run-main edu.ufl.cise.pipeline.Pipeline /tmp/mytempfull/x00 resources/test/kbaoutput/fx00' > /tmp/mytempfull/fx00.out 2>&1 &
+sbt 'run-main edu.ufl.cise.pipeline.Pipeline /tmp/mytempfull/x10 resources/test/kbaoutput/fx10' > /tmp/mytempfull/fx10.out 2>&1 &
+
