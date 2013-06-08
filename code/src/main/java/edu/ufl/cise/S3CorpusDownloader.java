f3810551f95ff2a7c2de1f3938e61bcfdbd6786a
Morteza Shahriari Nia <info@mshahriarinia.com>
2013-06-08 18:40:07 -0400

    Fixed bug on S3CorpusDownloader by process.destroy().

--- code/src/main/java/edu/ufl/cise/S3CorpusDownloader.java
+++ code/src/main/java/edu/ufl/cise/S3CorpusDownloader.java
@@ -1,27 +1,39 @@
 package edu.ufl.cise;
 
 import java.io.BufferedReader;
-import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.net.URLConnection;
 import java.util.Hashtable;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+/**
+ * Firstly, I would recommend replacing the line
+ * 
+ * Process process = Runtime.getRuntime ().exec ("/bin/bash");
+ * 
+ * with the lines
+ * 
+ * ProcessBuilder builder = new ProcessBuilder("/bin/bash");
+ * builder.redirectErrorStream(true); Process process = builder.start();
+ * 
+ * @author morteza
+ * 
+ */
 public class S3CorpusDownloader {
 
-	int						threadCount			= 32;
+	int						threadCount			= 5;
 	static String	localDirLaptop	= "/home/morteza/2013Corpus/s3.amazonaws.com/aws-publicdatasets/trec/kba/kba-streamcorpus-2013-v0_2_0-english-and-unknown-language/";
 	static String	localDirSDD			= "/media/sdd/s3.amazonaws.com/aws-publicdatasets/trec/kba/kba-streamcorpus-2013-v0_2_0-english-and-unknown-language/";
 	static String	localDirSDE			= "/media/sde/s3.amazonaws.com/aws-publicdatasets/trec/kba/kba-streamcorpus-2013-v0_2_0-english-and-unknown-language/";
-    static String   localDirTEST        = "/media/sde/tempStorage/";
+	static String	localDirTEST		= "/media/sde/tempStorage/";
 	static String	localDirPrefix	= "s3.amazonaws.com/aws-publicdatasets/trec/kba/kba-streamcorpus-2013-v0_2_0-english-and-unknown-language/";
 	String				AWS_URL					= "http://s3.amazonaws.com/aws-publicdatasets/trec/kba/kba-streamcorpus-2013-v0_2_0-english-and-unknown-language/";
 
@@ -67,79 +79,93 @@ public class S3CorpusDownloader {
 	private void download() {
 
 		//
-		// ExecutorService executor = Executors.newFixedThreadPool(threadCount);
+		ExecutorService executor = Executors.newFixedThreadPool(threadCount);
 		//
 
 		URL url;
 		InputStream is = null;
 		BufferedReader br;
+
+		int i = 0;
+		boolean finished = false;
+
+		Boolean TRUE = new Boolean(true);
+
+		final Hashtable<String, Boolean> alreadyDownloadedTable = new Hashtable<String, Boolean>();
+
+		{
+			List<String> alreadyDownloaded = null;
+			alreadyDownloaded = DirList.getFileList(localDirSDD, null);
+			for (String s : alreadyDownloaded) {
+				String tempFileStr = s.substring(localDirSDD.length());
+				// System.out.println(tempFileStr);
+				alreadyDownloadedTable.put(tempFileStr, TRUE);
+			}
+			alreadyDownloaded = DirList.getFileList(localDirSDE, null);
+			for (String s : alreadyDownloaded) {
+				String tempFileStr = s.substring(localDirSDE.length());
+				// System.out.println(tempFileStr);
+				alreadyDownloadedTable.put(tempFileStr, TRUE);
+
+				// System.out.println(s);
+				// alreadyDownloadedTable.put(s.substring(localDirSDE.length()), TRUE);
+			}
+			alreadyDownloaded = null;
+			alreadyDownloaded = DirList.getFileList(localDirLaptop, null);
+			for (String s : alreadyDownloaded) {
+				String tempFileStr = s.substring(localDirLaptop.length());
+				// System.out.println(tempFileStr);
+				alreadyDownloadedTable.put(tempFileStr, TRUE);
+
+				// System.out.println(s);
+				// alreadyDownloadedTable.put(s.substring(localDirLaptop.length()),
+				// TRUE);
+			}
+			alreadyDownloaded = null;
+		}
+
+		LinkedList<String> pageLines = new LinkedList<String>();
 		try {
 			url = new URL(
 					"http://s3.amazonaws.com/aws-publicdatasets/trec/kba/kba-streamcorpus-2013-v0_2_0-english-and-unknown-language/index.html");
 			is = url.openStream(); // throws an IOException
+
+			br = new BufferedReader(new InputStreamReader(is));
+			String line;
+			while (!finished) {
+				try {
+					line = br.readLine();
+
+					if (line == null)
+						finished = true;
+					else
+						pageLines.add(line);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+			br.close();
+			is.close();
 		} catch (MalformedURLException e1) {
 			e1.printStackTrace();
 		} catch (IOException e1) {
 			e1.printStackTrace();
 		}
-		br = new BufferedReader(new InputStreamReader(is));
 
-		int i = 0;
-		boolean finished = false;
-		String line;
-		Boolean TRUE = new Boolean(true);
-
-		final Hashtable<String, Boolean> alreadyDownloadedTable = new Hashtable<String, Boolean>();
-		{
-			List<String> alreadyDownloaded = null;
-			alreadyDownloaded = DirList.getFileList(localDirSDD, null);
-			for (String s : alreadyDownloaded) {
-                String tempFileStr = s.substring(localDirSDD.length());
-                //System.out.println(tempFileStr);
-				alreadyDownloadedTable.put(tempFileStr, TRUE);
-			}
-			alreadyDownloaded = DirList.getFileList(localDirSDE, null);
-			for (String s : alreadyDownloaded) {
-			     String tempFileStr = s.substring(localDirSDE.length());
-                //System.out.println(tempFileStr);
-				alreadyDownloadedTable.put(tempFileStr, TRUE);
-
-                //System.out.println(s);
-               // alreadyDownloadedTable.put(s.substring(localDirSDE.length()), TRUE);
-			}
-			alreadyDownloaded = null;
-			alreadyDownloaded = DirList.getFileList(localDirLaptop, null);
-			for (String s : alreadyDownloaded) {
-                 String tempFileStr = s.substring(localDirLaptop.length());
-                //System.out.println(tempFileStr);
-				alreadyDownloadedTable.put(tempFileStr, TRUE);
-
-				//System.out.println(s);
-                //alreadyDownloadedTable.put(s.substring(localDirLaptop.length()), TRUE);
-			}
-			alreadyDownloaded = null;
-		}
-
-		while (!finished) {
+		for (String tempLine : pageLines) {
 			Pattern p = Pattern.compile("a href=\"([^\"]+)\"");
 
-			try {
-				line = br.readLine();
+			final Matcher m1 = p.matcher(tempLine);
 
-				if (line == null)
-					finished = true;
-				else {
-					final Matcher m1 = p.matcher(line);
+			if (m1.find()) {
+				String linkStr = m1.group(1);
+				// System.out.println(linkStr);
+				final String dir = linkStr.substring(0, linkStr.indexOf('/'));
+				System.out.println(dir);
 
-					if (m1.find()) {
-						String linkStr = m1.group(1);
-						// System.out.println(linkStr);
-						final String dir = linkStr.substring(0, linkStr.indexOf('/'));
-						System.out.println(dir);
-
-						//
-						// Runnable worker = new Thread(i++ + " " + linkStr) {
-						// public void run() {
+				//
+				Runnable worker = new Runnable() {
+					public void run() {
 						//
 
 						// int size;
@@ -150,38 +176,31 @@ public class S3CorpusDownloader {
 							// downloadDir(localDirSDD, dir);
 							// else
 
-							//downloadDir(localDirSDE, dir, alreadyDownloadedTable);
-                            downloadDir(localDirTEST, dir, alreadyDownloadedTable);
+							// downloadDir(localDirSDE, dir, alreadyDownloadedTable);
 
+							downloadDir(localDirTEST, dir, alreadyDownloadedTable);
 
 						} catch (Exception e) {
 							e.printStackTrace();
 						}
 
 						//
-						// };
-						// };
-						// executor.execute(worker);
-						//
+					};
+				};
+				executor.execute(worker);
+				//
 
-					} else if (i > 1) {// skip initial no line
-						finished = true;
-					}
-				}
-			} catch (Exception e1) {
-				e1.printStackTrace();
 			}
 		}
-
 		//
-		// executor.shutdown();
-		// while (!executor.isTerminated()) {
-		// try {
-		// Thread.sleep(500);
-		// } catch (InterruptedException e) {
-		// e.printStackTrace();
-		// }
-		// }
+		executor.shutdown();
+		while (!executor.isTerminated()) {
+			try {
+				Thread.sleep(500);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
 		//
 
 		System.out.println("Finished all threads");
@@ -233,13 +252,34 @@ public class S3CorpusDownloader {
 		String line;
 
 		InputStream is;
+
 		URL url = new URL(AWS_URL + dir + "/index.html");
 		is = url.openStream();
 		BufferedReader br2 = new BufferedReader(new InputStreamReader(is));
 
 		Pattern p = Pattern.compile("a href=\"([^\"]+)\"");
-		while ((line = br2.readLine()) != null) {
-			Matcher m = p.matcher(line);
+		boolean finished = false;
+		LinkedList<String> pageLines = new LinkedList<String>();
+		while (!finished) {
+			try {
+				line = br2.readLine();
+				// System.out.println("line: " + line);
+				if (line == null)
+					finished = true;
+				else
+					pageLines.add(line);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+
+		br2.close();
+		is.close();
+
+		System.out.println("dir: " + dir);
+		for (String tempLine : pageLines) {
+
+			Matcher m = p.matcher(tempLine);
 			if (m.find()) {
 				String linkStr = m.group(1);
 				if (linkStr.contains("gpg")) {
@@ -262,15 +302,20 @@ public class S3CorpusDownloader {
 						// + localDir + dir + "/"
 						// + linkStr.substring(0, linkStr.length() - 4);
 
-						String command = commandWget;
-						System.out.println(command);
-						FileProcessor.runBinaryShellCommand(command);
+						Process process = Runtime.getRuntime().exec(commandWget);
+						
+						process.destroy();
+
+						// String command = commandWget;
+						// System.out.println(command);
+						// FileProcessor.runBinaryShellCommand(command);
 					} else {
 						System.out.println("Already Downloaded " + fileToFind);
 					}
 				}
 			}
 		}
+
 	}
 
 	/**
