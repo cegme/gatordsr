16051522a8f50eaa3b1dc26f604773ab1fc8dbf7
Christan Grant <christangrant@gmail.com>
2013-07-13 23:24:35 -0400

    This file holds the functions and feature scores for parameters

---
+++ code/cpp/Parameters.cc
@@ -0,0 +1,36 @@
+
+#include <algorithm>
+
+#include <boost/algorithm/string.hpp>
+
+#include "Parameters.h"
+
+std::map<std::string, double> const Parameters::qt_params = {
+  {"match", 100.0},
+  {"overlap", 50.0}
+};
+
+
+std::map<std::string, std::function<bool(QueryEntity, streamcorpus::Token)> > const Parameters::qt_functions = {
+  {"match", Parameters::qt_match},
+  {"overlap", Parameters::qt_overlap}
+};
+
+
+bool Parameters::qt_match(QueryEntity qe, streamcorpus::Token t) {
+  return std::any_of(qe.aliases.cbegin(), qe.aliases.cend(), [t] (std::string alias) {
+    return boost::iequals(t.token, alias);
+  });
+}
+
+
+bool Parameters::qt_overlap(QueryEntity qe, streamcorpus::Token t) {
+  typedef const boost::iterator_range<std::string::const_iterator> StringRange;
+
+  return std::any_of(qe.aliases.cbegin(), qe.aliases.cend(), [t] (std::string alias) {
+    return boost::ifind_first(
+      StringRange(t.token.begin(), t.token.end()),
+      StringRange(alias.begin(), alias.end()));
+    //return boost::ifind_first(t.token, alias) != std::string::npos;
+  });
+}
---
+++ code/cpp/Parameters.h
@@ -0,0 +1,30 @@
+
+#ifndef PARAMETERS_H
+#define PARAMETERS_H
+
+#include <functional>
+#include <map>
+#include <string>
+
+#include "streamcorpus_constants.h"
+#include "streamcorpus_types.h"
+
+
+#include "QueryEntity.h"
+
+class Parameters {
+
+private:
+  static bool qt_match(QueryEntity qe, streamcorpus::Token t);
+  static bool qt_overlap(QueryEntity qe, streamcorpus::Token t);
+
+
+public:
+  /** Parameters for searching for the propery entity refered by the query entity */
+  static std::map<std::string, double> const qt_params;
+
+  /** Functions for searching for the propery entity refered by the query entity */
+  static std::map<std::string, std::function<bool(QueryEntity, streamcorpus::Token)> > const qt_functions;
+};
+
+#endif  // PARAMETERS_H
