44ec8292b30c86df57dc32ff1fee8f1f8d3b6f7c
Christan Grant <christangrant@gmail.com>
2013-07-22 16:51:06 -0400

    Added code to merge the current index

---
+++ code/src/main/scala/edu/ufl/cise/util/treclucene/IndexMerger.scala
@@ -0,0 +1,71 @@
+package edu.ufl.cise.util.treclucene
+
+import scala.collection.JavaConversions._
+
+import java.io.File
+
+import org.apache.lucene.analysis.core.SimpleAnalyzer
+import org.apache.lucene.analysis.standard.StandardAnalyzer 
+import org.apache.lucene.document.Document
+import org.apache.lucene.document.Field
+import org.apache.lucene.document.IntField
+import org.apache.lucene.document.StringField
+import org.apache.lucene.document.TextField
+import org.apache.lucene.store.Directory
+import org.apache.lucene.store.NIOFSDirectory
+import org.apache.lucene.store.SimpleFSDirectory
+import org.apache.lucene.index.IndexWriter
+import org.apache.lucene.index.IndexWriterConfig
+import org.apache.lucene.util.Version
+import org.apache.thrift.protocol.TBinaryProtocol
+import org.apache.thrift.transport.TIOStreamTransport
+import org.apache.thrift.transport.TTransportException
+
+import edu.ufl.cise.Logging
+
+
+
+
+object IndexMerger extends Logging {
+
+
+  val OLD_INDEXES = "resources/lucene/indexdirs.txt"
+  val INDEX_DIR = "/media/sdc/optimizedindex"
+  
+  /** Command to find all index directories */
+  val cmd = """find /media/sdc/kbaindex/media/sd{d,e}/s3.amazonaws.com/aws-publicdatasets/trec/kba/kba-streamcorpus-2013-v0_2_0-english-and-unknown-language/????-??-??-??/ -type d """
+
+
+  def main (args: Array[String]) {
+    
+    // Get all the index files 
+    val analyzer = new StandardAnalyzer(Version.LUCENE_43)
+
+    logInfo("Fetching indexdirs to merge...")
+    val dirs = io.Source.fromFile(OLD_INDEXES).getLines.map(dirPath => new SimpleFSDirectory(new File(dirPath)))
+    val dirCount:Double = dirs.size
+    logInfo("Merging dirs: %d".format(dirCount))
+
+ 
+
+    val directory = new NIOFSDirectory(new File(INDEX_DIR))
+    val iwc = new IndexWriterConfig(Version.LUCENE_43, analyzer)
+    iwc.setRAMBufferSizeMB(3000)
+    
+    val writer = new IndexWriter(directory, iwc)
+
+    logInfo("Merging Added Indexes...\n ")
+    for ((dir,i) <- dirs.zipWithIndex) {
+      writer.addIndexes(dir)
+      System.err.println("\r%d percent complete".format(i/dirCount))
+    }
+    logInfo("\nClosing.")
+
+
+    // Close and wait for the merge to complete
+    writer.commit()
+    writer.close(true)
+  }
+
+
+}
