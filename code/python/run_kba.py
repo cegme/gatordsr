897290c2dbddd108c235fd43128b30fe38612ff7
Christan Grant <christangrant@gmail.com>
2013-08-15 15:35:05 -0400

    fixed the code to run the ccr task

--- code/python/run_kba.py
+++ code/python/run_kba.py
@@ -1,28 +1,35 @@
 #!/usr/bin/python
 
+import shlex
 import subprocess
+import sys
 
 
 # Function to run the CCR
 def ccr(infile, outfile):
   # TODO code to run the code
   # subprocess info http://docs.python.org/2/library/subprocess.html#subprocess.call
-  subprocess.call([ "sbt run-main edu.ufl.cise.kb.WikiAPI", ">", outfile])
+  cmd = shlex.split("sbt 'run-main edu.ufl.cise.kb.WikiAPI' > %s " % outfile)
+  print >> sys.stderr, "Running... %s" % ' '.join(cmd)
+  subprocess.call(cmd)
   pass
 
 
 # Function to run the ER
 def er(infile, outfile):
   # TODO code to run the code
-  print infile
-  subprocess.call(["echo", "'./er'", "infile", "outfile"])
+  cmd = shlex.split("echo 'run er thing'")
+  print >> sys.stderr, "Running... %s" % ' '.join(cmd)
+  subprocess.call(cmd)
   pass
 
 
 # Function to run the SSF
 def ssf(infile, outfile):
   # TODO code to run the code
-  subprocess.call(["echo", "'sbt run-main ...'"])
+  cmd = shlex.split("echo 'run ssf thing'")
+  print >> sys.stderr, "Running... %s" % ' '.join(cmd)
+  subprocess.call(cmd)
   pass
 
 
@@ -30,9 +37,9 @@ if __name__ == "__main__":
   import argparse
 
   parser = argparse.ArgumentParser(description='This is the file to execute the KBA program for the Gator DSR team')
-  parser.add_argument("ccrout", metavar="ccr_file", type=str, default="/tmp/ccr", help="This is the output file for the ccr step")
-  parser.add_argument("erout", metavar="cr_file", type=str, default="/tmp/ssf", help="This is the output file for the er step")
-  parser.add_argument("ssfout", metavar="ssf_file", type=str, default="/tmp/ssf", help="This is the output file for the ssf step")
+  parser.add_argument("--ccrout", metavar="ccr_file", type=str, default="/tmp/ccr", nargs=1, help="This is the output file for the ccr step")
+  parser.add_argument("--erout", metavar="cr_file", type=str, default="/tmp/ssf", nargs=1, help="This is the output file for the er step")
+  parser.add_argument("--ssfout", metavar="ssf_file", type=str, default="/tmp/ssf", nargs=1, help="This is the output file for the ssf step")
 
   args = parser.parse_args()
 
