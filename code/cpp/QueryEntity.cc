5e26b3e5b0d84d03f1a5ea91f48cbacc2b933c10
Christan Grant <christangrant@gmail.com>
2013-08-21 04:31:40 -0400

    changes so the code compiles on sm321

--- code/cpp/QueryEntity.cc
+++ code/cpp/QueryEntity.cc
@@ -103,7 +103,7 @@ std::string QueryEntity::getWikiBody(const char *page_title) {
   char cmd[512];
 
   // Create a tmp file name for the webpage
-  std::tmpnam(tmpfilenam);
+  mkstemp(tmpfilenam);
   //log_debug("tmpfilenam: %s", tmpfilenam);
   
   std::snprintf(cmd, 512, wiki_page, tmpfilenam, page_title);
