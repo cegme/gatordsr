eae8626300afb174da37318bc5e148d133a4c960
Christan Grant <christangrant@gmail.com>
2013-08-26 13:45:25 -0400

    moved the \r to the beginning of the log line

--- code/cpp/Util.h
+++ code/cpp/Util.h
@@ -1,4 +1,3 @@
-
 #ifndef UTIL_H
 #define UTIL_H
 
@@ -74,15 +73,15 @@ static std::vector<std::string> tok(std::string str, std::string sep) {
 #define MAX(a,b) ( ((a) > (b)) ? (a) : (b) )
 
 // Do a carrage return after these lines so they will rewrite echother
-#define log_info_r(M, ...) fprintf(stderr, "%s [INFO] (%s:%d) | " M "\r", DATE_STRING,  __FILE__, __LINE__, ##__VA_ARGS__)
+#define log_info_r(M, ...) fprintf(stderr, "\r%s [INFO] (%s:%d) | " M , DATE_STRING,  __FILE__, __LINE__, ##__VA_ARGS__)
 
-#define log_trace_r(M, ...) fprintf(stderr, "%s [trace] (%s:%d) | " M "\r", DATE_STRING,  __FILE__, __LINE__, ##__VA_ARGS__)
+#define log_trace_r(M, ...) fprintf(stderr, "\r%s [trace] (%s:%d) | " M , DATE_STRING,  __FILE__, __LINE__, ##__VA_ARGS__)
 
-#define log_debug_r(M, ...) fprintf(stderr, "%s [DEBUG] (%s:%d) | " M "\r", DATE_STRING,  __FILE__, __LINE__, ##__VA_ARGS__)
+#define log_debug_r(M, ...) fprintf(stderr, "\r%s [DEBUG] (%s:%d) | " M , DATE_STRING,  __FILE__, __LINE__, ##__VA_ARGS__)
 
-#define log_err_r(M, ...) fprintf(stderr, "%s [ERROR] (%s:%d: errno: %s) | " M "\r", DATE_STRING, __FILE__, __LINE__, clean_errno(), ##__VA_ARGS__)
+#define log_err_r(M, ...) fprintf(stderr, "\r%s [ERROR] (%s:%d: errno: %s) | " M , DATE_STRING, __FILE__, __LINE__, clean_errno(), ##__VA_ARGS__)
 
-#define log_warn_r(M, ...) fprintf(stderr, "%s [WARN] (%s:%d: errno: %s) | " M "\r", DATE_STRING, __FILE__, __LINE__, clean_errno(), ##__VA_ARGS__)
+#define log_warn_r(M, ...) fprintf(stderr, "\r%s [WARN] (%s:%d: errno: %s) | " M , DATE_STRING, __FILE__, __LINE__, clean_errno(), ##__VA_ARGS__)
 
 
 
