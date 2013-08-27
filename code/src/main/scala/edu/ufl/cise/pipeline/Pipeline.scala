36e7e85f83a2fbc8d1736afe0aa02f8702d60fab
SunPHM <pengyang823@gmail.com>
2013-08-27 13:44:27 +0800

    Update Pipeline.scala

--- code/src/main/scala/edu/ufl/cise/pipeline/Pipeline.scala
+++ code/src/main/scala/edu/ufl/cise/pipeline/Pipeline.scala
@@ -371,7 +371,7 @@ object Pipeline extends Logging {
             val txt = SimpleJob.transform(tokens.slice(entity.end + 1, target.begin));
             if(txt.contains(date) && (target.entity_type.equals("TIME") || target.entity_type.equals("DATE"))) {
               // generate results and output
-              val comment = "# " + entity.content + " " + text + " " + target.content + " --- " + SimpleJob.transform(tokens);
+              val comment = "# " + entity.content + " " + txt + " " + target.content + " --- " + SimpleJob.transform(tokens);
               val byte_range = getByteRange(target, tokens);
               KBAOutput.add(array(6), entity.topic_id, 1000, array(0), "DateOfDeath", tokens(target.begin).equiv_id, byte_range, comment, array)
             }
@@ -389,7 +389,7 @@ object Pipeline extends Logging {
           if(txt.contains(contact) && (target.entity_type.equals("TIME") || target.entity_type.equals("DATE") || 
             target.entity_type.equals("FAC"))) {
             // generate results and output
-            val comment = "# " + entity.content + " " + text + " " + target.content + " --- " + SimpleJob.transform(tokens);
+            val comment = "# " + entity.content + " " + txt + " " + target.content + " --- " + SimpleJob.transform(tokens);
             val byte_range = getByteRange(target, tokens);
             KBAOutput.add(array(6), entity.topic_id, 1000, array(0), "Contact_Meet_PlaceTime", tokens(target.begin).equiv_id, byte_range, comment, array)
           }
